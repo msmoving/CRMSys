8380c0eba20332a6dad2c79af96c5475985e2aa0
msmoving <fchms@sina.com>
2015-12-20 22:18:06 +0800

    testpull2
    testpull2

--- src/com/meng/git/GitTest.java
+++ src/com/meng/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("remote server pulltest01!");
+		System.out.println("remote server pulltest02!");
 	}
 }
